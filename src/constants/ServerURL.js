750a07f21187df9141b082674a0cde567799dd2c
pipi-shortstocking <jannie435@naver.com>
2024-10-30 23:06:55 +0900

    test

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,3 +1,3 @@
 // export const SERVER_URL = "http://localhost:8080/";
 // export const SERVER_URL = "http://13.125.61.13:8080/";
-export const SERVER_URL = "http://www.podo-store.com/";
+export const SERVER_URL = "http://www.pipishortstocking.shop/api/";
